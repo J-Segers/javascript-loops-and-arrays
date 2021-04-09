531bf5ba921b5487244d826cbdf933ed63973059
J-Segers <jethroseger@gmail.com>
2021-04-09 17:13:04 +0200

    even opnieuw gemaakt was de branch vergeten te maken

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -4,7 +4,7 @@
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 namen zou bevatten!
 // ==========================================
 
-const names = ["Henk", "Piet", "Fred", "Joop"];
+let names = ["Henk", "Piet", "Fred", "Joop"];
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -12,6 +12,11 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + "je";
+}
+
+console.log(names);
 
 // ==========================================
 // Opdracht 2
@@ -27,6 +32,9 @@ const numbers = [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
 
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] *= 3;
+}
 
 // ==========================================
 // Opdracht 3
@@ -42,3 +50,11 @@ const squares = [30, 2, 8, 24, 11];
 // console.log(numbers) geeft: [30, 2, 8, 24, 11];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(numbers) geeft: [27000, 8, 512, 13824, 1331];
+
+console.log(squares);
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = squares[i] ** 3;
+}
+
+console.log(squares);
